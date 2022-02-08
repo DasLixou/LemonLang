0b9221afba28577ac9d801790c926d7d8dc088ab
DasLixou <einlixou@gmail.com>
2022-02-08 19:45:38 +0100

    🙀 Extended Parser

--- src/include/parser.h
+++ src/include/parser.h
@@ -6,10 +6,18 @@
 typedef struct PARSER_STRUCT
 {
     lexer_T *lexer;
+    token_T *token;
 } parser_T;
 
 parser_T *init_parser(lexer_T *lexer);
 
 void parser_parse(parser_T *parser);
+
+// Parser Additions //
+void parse_statement(parser_T *parser);
+
+// Parser Utils //
+token_T *parser_eat(parser_T *parser, int type);
+token_T *parser_continue(parser_T *parser);
 
 #endif // LEMON_PARSER_H
--- src/parser.c
+++ src/parser.c
@@ -4,15 +4,43 @@ parser_T *init_parser(lexer_T *lexer)
 {
     parser_T *parser = calloc(1, sizeof(struct PARSER_STRUCT));
     parser->lexer = lexer;
+    parser->token = lexer_next_token(parser->lexer);
 
     return parser;
 }
 
 void parser_parse(parser_T *parser)
 {
-    token_T *token = calloc(1, sizeof(struct TOKEN_STRUCT));
-    while ((token = lexer_next_token(parser->lexer))->type != TOKEN_EOF)
+    while (parser->token->type != TOKEN_EOF)
     {
-        printf("%s\n", token_to_str(token));
+        printf("%s\n", token_to_str(parser->token));
+        parse_statement(parser);
+        parser_continue(parser);
     }
+}
+
+// Parser Additions //
+
+void parse_statement(parser_T *parser)
+{
+}
+
+// Parser Utils //
+
+token_T *parser_eat(parser_T *parser, int type)
+{
+    if (parser->token->type != type)
+    {
+        printf("[Parser]: Unexpected token: `%s`, was expecting: `%s`\n", token_to_str(parser->token), token_type_to_str(type));
+        exit(1);
+    }
+
+    parser->token = lexer_next_token(parser->lexer);
+    return parser->token;
+}
+
+token_T *parser_continue(parser_T *parser)
+{
+    parser->token = lexer_next_token(parser->lexer);
+    return parser->token;
 }
