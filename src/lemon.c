0a22a41fdc64623052063ddb1b0dee6c544db044
DasLixou <einlixou@gmail.com>
2022-02-08 17:19:34 +0100

    🧩 Playing around with Tokens

--- src/lemon.c
+++ src/lemon.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
 #include "include/parser.h"
+#include "include/token.h"
 
 int main()
 {
-    printf("[lemonc] Start compiling...");
-    lexer_T *lexer = init_lexer("");
+    printf("[lemonc] Start compiling...\n");
+    lexer_T *lexer = init_lexer("i = 1;");
     parser_T *parser = init_parser(lexer);
+
+    token_T *token = init_token("i", TOKEN_ID);
+    printf("%s", token_to_str(token));
 }
---
+++ src/token.c
@@ -0,0 +1,50 @@
+#include "include/token.h"
+
+token_T *init_token(char *value, int type)
+{
+    token_T *token = calloc(1, sizeof(struct TOKEN_STRUCT));
+    token->value = value;
+    token->type = type;
+
+    return token;
+}
+
+const char *token_type_to_str(int type)
+{
+    switch (type)
+    {
+    case TOKEN_ID:
+        return "ID";
+    case TOKEN_EQUALS:
+        return "EQUALS";
+    case TOKEN_LPAREN:
+        return "LPAREN";
+    case TOKEN_RPAREN:
+        return "RPAREN";
+    case TOKEN_LBRACE:
+        return "LBRACE";
+    case TOKEN_RBRACE:
+        return "RBRACE";
+    case TOKEN_COMMA:
+        return "COMMA";
+    case TOKEN_INT:
+        return "INT";
+    case TOKEN_SEMICOLON:
+        return "SEMICOLON";
+    case TOKEN_EOF:
+        return "EOF";
+    default:
+        return "! UNKNOWN TOKEN !";
+    }
+}
+
+char *token_to_str(token_T *token)
+{
+    const char *type_str = token_type_to_str(token->type);
+    const char *template = "<type=`%s`, int_type=`%d`, value=`%s`>";
+
+    char *str = calloc(strlen(type_str) + strlen(template) + strlen(token->value), sizeof(char));
+    sprintf(str, template, type_str, token->type, token->value);
+
+    return str;
+}
