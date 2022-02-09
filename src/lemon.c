b986791471b5bb3bb4113b7f091c551dd6298423
DasLixou <einlixou@gmail.com>
2022-02-09 16:37:56 +0100

    🀄 Added file parameter for compiling

--- src/lemon.c
+++ src/lemon.c
@@ -4,10 +4,15 @@
 #include "include/token.h"
 #include "include/io.h"
 
-int main()
+int main(int argc, char *argv[])
 {
+    if (argv[1] == NULL)
+    {
+        printf("[lemonc] Please provide a file to compile.");
+        return 1;
+    }
     printf("[lemonc] Start compiling...\n");
-    lexer_T *lexer = init_lexer(io_read_file("example.lemon"));
+    lexer_T *lexer = init_lexer(io_read_file(argv[1]));
     parser_T *parser = init_parser(lexer);
     parser_parse(parser);
 }
