1bd2687d76c773edf97babfdb4502f304535fbbf
DasLixou <einlixou@gmail.com>
2022-02-16 18:35:26 +0100

    🪦 Removed all C files

--- src/AST.c
+++
@@ -1,9 +0,0 @@
-#include "include/AST.h"
-
-AST_T *init_ast(int type)
-{
-    AST_T *ast = calloc(1, sizeof(struct AST_STRUCT));
-    ast->type = type;
-
-    return ast;
-}
--- src/include/io.h
+++
@@ -1,6 +0,0 @@
-#ifndef LEMON_IO_H
-#define LEMON_IO_H
-
-char *io_read_file(const char *filename);
-void io_write_file(const char *filename, char *outbuffer);
-#endif
